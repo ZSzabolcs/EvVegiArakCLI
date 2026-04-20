890f77fad22eb94e18bd3666f5c6f34c50d1a7ba
ZSzabolcs <zelenaksz@kkszki.hu>
2026-04-20 10:50:11 +0200

    1. feladat

---
+++ EvVegiArakCLI/Arak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvVegiArakCLI
+{
+    internal class Arak
+    {
+        public int December { get; private set; }
+        public int Kod { get; private set; }
+        public string Megnevezes { get; private set; }
+        public int November { get; private set; }
+        public int Oktober { get; private set; }
+        public int Szeptember { get; private set; }
+
+        public Arak(string sor)
+        {
+            string[] temp = sor.Split(';');
+            Kod = int.Parse(temp[0]);
+            Megnevezes = temp[1].Split(',')[0];
+            Szeptember = int.Parse(temp[2]);
+            Oktober = int.Parse(temp[3]);
+            November = int.Parse(temp[4]);
+            December = int.Parse(temp[5]);
+        }
+    }
+}
