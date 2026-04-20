d6e9144cdafef40990889b90386cb0c958711c15
ZSzabolcs <zelenaksz@kkszki.hu>
2026-04-20 10:28:40 +0200

    Létrehozás

---
+++ EvVegiArakCLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EvVegiArakCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
